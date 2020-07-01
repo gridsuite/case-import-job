ea07c82651d5b24c9dc58651aeceb8f42c513ea5
HOMER Etienne <etiennehomer@gmail.com>
2020-07-01 15:40:39 +0200

    Remove oldest files (keep only the 100 last ones)
    Signed-off-by: HOMER Etienne <etiennehomer@gmail.com>

--- case-import-emulator/deposit_case_file.sh
+++ case-import-emulator/deposit_case_file.sh
@@ -3,15 +3,16 @@ echo "Import new case"
 
 importDate=$(date +"%Y%m%d_%H%M")
 echo "Import date = $importDate"
+fileToImport="example.xml"
 
-fileName=$(basename -- "cases/example.xml")
+fileName=$(basename -- "cases/$fileToImport")
 fileName="${fileName%.*}"
 echo "Filename = $fileName"
 
 uniqueFileName="${importDate}_SN5_D80.xml"
 echo "uniqueFileName = $uniqueFileName"
 
-fileToImport="LF.xml"
+
 cp cases/$fileToImport ~/opde/$uniqueFileName
 
 caseDate=$(date +"%Y-%m-%dT%T.000+01:00")
@@ -21,3 +22,16 @@ echo "Case date = $caseDate"
 xsltproc --stringparam dateReplacement "$caseDate" changedate.xsl ~/opde/$uniqueFileName > ~/opde/"$uniqueFileName.tmp"
 rm ~/opde/$uniqueFileName
 mv ~/opde/"$uniqueFileName.tmp" ~/opde/$uniqueFileName
+
+#Clean old cases
+filesCount=$(ls -1q ~/opde* | wc -l)
+while [ $filesCount -gt 100 ] 
+do
+	#remove oldest file
+	oldestFile=$(find ~/opde -type f | sort | head -n 1)
+	rm $oldestFile
+	filesCount=$[$filesCount - 1]
+done
+
+
+
