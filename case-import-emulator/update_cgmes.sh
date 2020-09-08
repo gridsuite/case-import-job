fe21770fe9c237ee70f202bd87eaca1886fb8f40
Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>
2020-09-08 10:45:01 +0200

    CGMES version of the emulator (#7)
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>

---
+++ case-import-emulator/update_cgmes.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+cgmesPath=$1
+country=$2
+outputDir=$3
+version=1
+
+# generate a new date for the CGMES
+caseDateIso=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+caseDate=$(date -d $caseDateIso +"%Y%m%d_%H%M")
+dayOfTheWeek=$(date -d $caseDateIso '+%u')
+
+# create a working directory
+tmpDir=$(mktemp -d)
+cd $tmpDir
+
+# copy the CGMES to the working directory
+cp $cgmesPath $tmpDir
+
+# unzip the CGMES
+zipFile=$(basename $1)
+unzip *.zip >> /dev/null
+
+# for each of the profile replace scenarioTime and version
+caseName=${zipFile%%.zip}
+cd $caseName
+for xmlFile in $(ls *.xml); do
+    sed -i -e "s/<md:Model.scenarioTime>.*<\/md:Model.scenarioTime>/<md:Model.scenarioTime>$caseDateIso<\/md:Model.scenarioTime>/g" $xmlFile
+    sed -i -e "s/<md:Model.version>.*<\/md:Model.version>/<md:Model.version>$version<\/md:Model.version>/g" $xmlFile
+done
+
+# rebuild the zip with update profiles and using a new naming
+cd ..
+rm $zipFile
+newZipFile=${caseDate}_FO${dayOfTheWeek}_${country}${version}
+zip -j $newZipFile $caseName/* >> /dev/null
+
+# copy the new CGMES to output directory
+rm -f $outputDir/${newZipFile}.zip
+mv ${newZipFile}.zip $outputDir
+
+# delete the working directory
+rm -rf $tmpDir
