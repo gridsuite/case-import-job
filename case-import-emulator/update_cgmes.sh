d502b20fb79690e832684a48278abbe082dd1b04
Franck LECUYER <franck.lecuyer@rte-france.com>
2020-11-03 14:14:29 +0100

    Changing the uuids of SV, TP, EQ, SSH profiles in the FullModel part Changing the dependencies uuids in the FullModel part
    Signed-off-by: Franck LECUYER <franck.lecuyer@rte-france.com>

--- case-import-emulator/update_cgmes.sh
+++ case-import-emulator/update_cgmes.sh
@@ -9,6 +9,33 @@ then
 fi
 version=001
 
+# update uuid in xml profile
+# $1 is the new uuid
+# $2 is the xml file name
+updateUuid() {
+  sed -i -e "s/\(<md:FullModel[ ]*rdf:about=\"urn:uuid:\).*\(\">\)/\1$1\2/g" $2
+}
+
+# update scenario time, created and version in xml profile
+# $1 is the case date in iso format
+# $2 is the version
+# $3 is the xml file name
+updateTimeAndVersion()
+{
+  sed -i -e "s/<md:Model.scenarioTime>.*<\/md:Model.scenarioTime>/<md:Model.scenarioTime>$1<\/md:Model.scenarioTime>/g" $3
+  sed -i -e "s/<md:Model.version>.*<\/md:Model.version>/<md:Model.version>$2<\/md:Model.version>/g" $3
+  sed -i -e "s/<md:Model.created>.*<\/md:Model.created>/<md:Model.created>$1<\/md:Model.created>/g" $3
+}
+
+# update dependencies uuids in xml profile
+# $1 is the old uuid
+# $2 is the new uuid
+# $3 is the xml file name
+updateDependencies() {
+  sed -i -e "s/\(<md:Model.DependentOn[ ]*rdf:resource=\"urn:uuid:\)$1\(.*\)/\1$2\2/g" $3
+}
+
+
 # generate a new date for the CGMES
 caseDateIso=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 caseDate=$(date -d $caseDateIso +"%Y%m%dT%H%MZ")
@@ -33,9 +60,9 @@ if [[ "${nbXml}" -eq 1 ]]
 then
   # individual zip profile file as input
   for xmlFile in $(ls *.xml); do
-    sed -i -e "s/<md:Model.scenarioTime>.*<\/md:Model.scenarioTime>/<md:Model.scenarioTime>$caseDateIso<\/md:Model.scenarioTime>/g" $xmlFile
-    sed -i -e "s/<md:Model.version>.*<\/md:Model.version>/<md:Model.version>$version<\/md:Model.version>/g" $xmlFile
-    sed -i -e "s/<md:Model.created>.*<\/md:Model.created>/<md:Model.created>$caseDateIso<\/md:Model.created>/g" $xmlFile
+    newUuid=$(uuidgen)
+    updateTimeAndVersion $caseDateIso $version $xmlFile
+    updateUuid ${newUuid} $xmlFile
   done
   rm $zipFile
   newZipFile=${caseDate}_${zipFile#*_}
@@ -49,6 +76,31 @@ else
     cd $caseName
   fi
 
+  for xmlFile in $(ls *.xml); do
+    # memorizing old and new uuid, and setting new uuid for SV, TP, EQ, SSH profiles
+    if [[ "$xmlFile" =~ ^.*_(SV)_[0-9]+[.]xml$ ]]
+    then
+      oldUuidSV=$(grep 'md:FullModel.*rdf:about=' $xmlFile | cut -d'"' -f 2 | cut -d':' -f 3)
+      newUuidSV=$(uuidgen)
+      sed -i -e "s/\(<md:FullModel[ ]*rdf:about=\"urn:uuid:\).*\(\">\)/\1${newUuidSV}\2/g" $xmlFile
+    elif [[ "$xmlFile" =~ ^.*_(TP)_[0-9]+[.]xml$ ]]
+    then
+      oldUuidTP=$(grep 'md:FullModel.*rdf:about=' $xmlFile | cut -d'"' -f 2 | cut -d':' -f 3)
+      newUuidTP=$(uuidgen)
+      sed -i -e "s/\(<md:FullModel[ ]*rdf:about=\"urn:uuid:\).*\(\">\)/\1${newUuidTP}\2/g" $xmlFile
+    elif [[ "$xmlFile" =~ ^.*_(EQ)_[0-9]+[.]xml$ ]]
+    then
+      oldUuidEQ=$(grep 'md:FullModel.*rdf:about=' $xmlFile | cut -d'"' -f 2 | cut -d':' -f 3)
+      newUuidEQ=$(uuidgen)
+      sed -i -e "s/\(<md:FullModel[ ]*rdf:about=\"urn:uuid:\).*\(\">\)/\1${newUuidEQ}\2/g" $xmlFile
+    elif [[ "$xmlFile" =~ ^.*_(SSH)_[0-9]+[.]xml$ ]]
+    then
+      oldUuidSSH=$(grep 'md:FullModel.*rdf:about=' $xmlFile | cut -d'"' -f 2 | cut -d':' -f 3)
+      newUuidSSH=$(uuidgen)
+      sed -i -e "s/\(<md:FullModel[ ]*rdf:about=\"urn:uuid:\).*\(\">\)/\1${newUuidSSH}\2/g" $xmlFile
+    fi
+  done
+
   if [[ "${outputType}" == "INDIVIDUAL" ]]
   then
     # generating individual profile zip files for SV, TP, SSH and EQ
@@ -56,10 +108,12 @@ else
       # we only deal with SV, TP, SSH and EQ profiles
       if [[ "$xmlFile" =~ ^.*_(SV|TP|EQ|SSH)_[0-9]+[.]xml$ ]]
       then
-        # for each of the profile replace scenarioTime, version and created
-        sed -i -e "s/<md:Model.scenarioTime>.*<\/md:Model.scenarioTime>/<md:Model.scenarioTime>$caseDateIso<\/md:Model.scenarioTime>/g" $xmlFile
-        sed -i -e "s/<md:Model.version>.*<\/md:Model.version>/<md:Model.version>$version<\/md:Model.version>/g" $xmlFile
-        sed -i -e "s/<md:Model.created>.*<\/md:Model.created>/<md:Model.created>$caseDateIso<\/md:Model.created>/g" $xmlFile
+        # for each of the profile replace scenarioTime, version, created and dependencies with new uuids
+        updateTimeAndVersion $caseDateIso $version $xmlFile
+        updateDependencies ${oldUuidSV} ${newUuidSV} $xmlFile
+        updateDependencies ${oldUuidTP} ${newUuidTP} $xmlFile
+        updateDependencies ${oldUuidEQ} ${newUuidEQ} $xmlFile
+        updateDependencies ${oldUuidSSH} ${newUuidSSH} $xmlFile
 
         zipFile=${xmlFile%%.xml}
         newZipFile=${caseDate}_${zipFile#*Z_}
@@ -90,9 +144,12 @@ else
   then
       # generating complete cgmes zip file
       for xmlFile in $(ls *.xml); do
-        sed -i -e "s/<md:Model.scenarioTime>.*<\/md:Model.scenarioTime>/<md:Model.scenarioTime>$caseDateIso<\/md:Model.scenarioTime>/g" $xmlFile
-        sed -i -e "s/<md:Model.version>.*<\/md:Model.version>/<md:Model.version>$version<\/md:Model.version>/g" $xmlFile
-        sed -i -e "s/<md:Model.created>.*<\/md:Model.created>/<md:Model.created>$caseDateIso<\/md:Model.created>/g" $xmlFile
+        # for each of the profile replace scenarioTime, version, created and dependencies with new uuids
+        updateTimeAndVersion $caseDateIso $version $xmlFile
+        updateDependencies ${oldUuidSV} ${newUuidSV} $xmlFile
+        updateDependencies ${oldUuidTP} ${newUuidTP} $xmlFile
+        updateDependencies ${oldUuidEQ} ${newUuidEQ} $xmlFile
+        updateDependencies ${oldUuidSSH} ${newUuidSSH} $xmlFile
       done
 
       # rebuild the zip with update profiles and using a new naming
